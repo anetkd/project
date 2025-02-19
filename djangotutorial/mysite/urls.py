e12a36f50fc371b65fa458b5e8a0216a402e5c67
anetkd <101451818+anetkd@users.noreply.github.com>
2025-02-19 10:16:46 +0000

    g

---
+++ djangotutorial/mysite/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+from polls.models import Choice
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
---
+++ djangotutorial/polls/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PollsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "polls"
+# https://django-plotly-dash.readthedocs.io/en/latest/installation.html 
+# https://www.django-rest-framework.org/ эта нужна
