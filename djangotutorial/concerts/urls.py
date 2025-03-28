13d554c42a753997a0c3c24ca0e02f3e70e84500
anetkd <101451818+anetkd@users.noreply.github.com>
2025-03-28 12:13:21 +0000

    e

--- djangotutorial/concerts/urls.py
+++ djangotutorial/concerts/urls.py
@@ -1,8 +1,10 @@
 # concerts/urls.py
 from django.urls import path
-from .views import concert_list, concert_detail
+from .views import concert_list, concert_detail, concert_report, create_concert
 
 urlpatterns = [
     path('', concert_list, name='concert_list'),
     path('concert/<int:concert_id>/', concert_detail, name='concert_detail'),
+    path('report/', concert_report, name='concert_report'),
+    path('concert/create/', create_concert, name='create_concert'),
 ]
--- djangotutorial/concerts/views.py
+++ djangotutorial/concerts/views.py
@@ -1,6 +1,9 @@
 # concerts/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ConcertForm
 from .models import Concert
+import plotly.express as px
+import pandas as pd
 
 def concert_list(request):
     concerts = Concert.objects.all()
@@ -9,3 +12,28 @@ def concert_list(request):
 def concert_detail(request, concert_id):
     concert = get_object_or_404(Concert, id=concert_id)
     return render(request, 'concerts/concert_detail.html', {'concert': concert})
+
+def concert_report(request):
+    concerts = Concert.objects.all()
+    data = {
+        'Artist': [concert.artist for concert in concerts],
+        'Date': [concert.date for concert in concerts],
+        'Venue': [concert.venue for concert in concerts],
+        'Price': [concert.price for concert in concerts],
+    }
+    df = pd.DataFrame(data)
+
+    fig = px.bar(df, x='Artist', y='Price', color='Venue', title='Concert Prices by Artist')
+    graph = fig.to_html(full_html=False)
+
+    return render(request, 'concerts/report.html', {'graph': graph})
+
+def create_concert(request):
+    if request.method == 'POST':
+        form = ConcertForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('concert_list')
+    else:
+        form = ConcertForm()
+    return render(request, 'concerts/create_concert.html', {'form': form})
